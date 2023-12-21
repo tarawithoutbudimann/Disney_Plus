2c80249e22bf01b043d2a4649b08c30afcd1f572
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2023-12-22 01:21:55 +0700

    crud

---
+++ app/src/main/java/com/example/netpliks/Admin.kt
@@ -0,0 +1,4 @@
+package com.example.netpliks
+
+class Admin {
+}
---
+++ app/src/main/java/com/example/netpliks/AdminEdit.kt
@@ -0,0 +1,11 @@
+package com.example.netpliks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_edit)
+    }
+}
